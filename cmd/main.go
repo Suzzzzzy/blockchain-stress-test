c2fe01870ddc5c4ca3b47886aaa63c8f59d1c320
Suzzzzzy <suji9709@gmail.com>
2024-05-23 16:16:21 +0900

    add 블록 추가기능 main에 추가

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"blockchain/internal/blockchain"
 	"blockchain/internal/database"
 	"blockchain/internal/util"
 )
@@ -20,4 +21,18 @@ func main() {
 		logger.Info("database migration completed successfully")
 	}
 
+	// 블록체인 생성
+	bc := blockchain.NewBlockchain()
+	// 트랜잭션 생성
+	tx1 := blockchain.NewTransaction("addr1", "add2", 10.0)
+	tx2 := blockchain.NewTransaction("addr3", "addr4", 20.0)
+	// 블록 생성 및 추가
+	block := blockchain.NewBlock("0", []*database.Transaction{tx1, tx2})
+	bc.AddBlock(block)
+	// 데이터베이스에 블록 저장
+	dbHandler := database.NewDatabaseManager(db)
+	err = dbHandler.AddBlock(block)
+	if err != nil {
+		logger.Error("failed to save block to db: " + err.Error())
+	}
 }
